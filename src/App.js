157716a9b48e53eef3d0cfca94af34117fcb296a
jochenruland <jochenhgruland@gmail.com>
2023-03-16 09:25:41 +0100

    ADD new components MyForm StateManager LifecycleMethods

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 import ControlledInput from './components/ControlledInput';
+import MyForm from './components/MyForm';
+import StateManager from './components/StateManager';
+import LifecycleMethods from './components/LifecycleMethods';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <Counter />
-          <ControlledInput />
+        
+          <MyForm />
+          <StateManager />
+          <LifecycleMethods />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
